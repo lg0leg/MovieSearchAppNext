81853554f368153c4880058c517988eb3cf351f0
lg0leg <lg.oleg1067@gmail.com>
2025-04-15 20:14:01 +0300

    feat: improve Movie page

--- movie-search-app/app/movies/[id]/page.js
+++ movie-search-app/app/movies/[id]/page.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import '../../../styles/movie.scss';
 import { useParams } from 'next/navigation';
-import { AspectRatio, Container, Divider, Flex, Grid, Group, Image, Space, Stack, Text, Title } from '@mantine/core';
+import { AspectRatio, Container, Divider, Flex, Group, Image, Space, Stack, Text, Title } from '@mantine/core';
 import { getDataFromApi } from '../../../src/utils/api';
 import Favorites from '../../../src/components/favorites/favorites';
 
@@ -103,14 +103,15 @@ export default function Movie() {
   const genres = movieInfo.genres.map((item) => item.name).join(', ');
 
   return (
-    <Container style={{ paddingTop: 40, paddingBottom: 40 }}>
-      <Group className="movie-info-card" align="flex-start" w={800}>
-        <AspectRatio ratio={250 / 352} w={250}>
+    <Container style={{ paddingTop: 40, paddingBottom: 40, justifyItems: 'center' }}>
+      <Group className="movie-info-card" align="flex-start" w={'100%'} maw={800} wrap="nowrap">
+        <Image className="movie-info-bg-image" src={`https://image.tmdb.org/t/p/original/${movieInfo.poster_path}`} fallbackSrc="/noPoster.png" alt={`poster`} />
+        <AspectRatio className="movie-info-image" ratio={250 / 352} w={250} miw={250}>
           <Image src={`https://image.tmdb.org/t/p/original/${movieInfo.poster_path}`} fallbackSrc="/noPoster.png" alt={`${movieInfo.original_title} poster`} />
         </AspectRatio>
-        <Stack justify="space-between" h={352}>
+        <Stack justify="space-between" mih={352}>
           <Flex direction="column">
-            <Title order={2} c={'#9854f6'}>
+            <Title className="movie-info-title" order={2} c={'#9854f6'} lineClamp={3}>
               {movieInfo.original_title}
             </Title>
             <Text mt={8} mb={8} c={'#7b7c88'}>
@@ -122,45 +123,39 @@ export default function Movie() {
               <Text c={'#7b7c88'}>({movieInfo.vote_count})</Text>
             </Flex>
           </Flex>
-          <Grid cols={2} w={442} gutter>
-            <Grid.Col span={4}>
-              <Flex direction="column" gap={12}>
-                <Text c={'#7b7c88'}>Duration</Text>
-                <Text c={'#7b7c88'}>Premiere</Text>
-                <Text c={'#7b7c88'}>Budget</Text>
-                <Text c={'#7b7c88'}>Gross worldwide</Text>
-                <Text c={'#7b7c88'}>Genres</Text>
-              </Flex>
-            </Grid.Col>
-            <Grid.Col span={'auto'}>
-              <Flex direction="column" gap={12}>
-                <Text>{duration}</Text>
-                <Text>{premiereDate}</Text>
-                <Text>{budget}</Text>
-                <Text>{revenue}</Text>
-                <Text>{genres}</Text>
-              </Flex>
-            </Grid.Col>
-          </Grid>
+          <div className="movie-info-details">
+            <Text c={'#7b7c88'}>Duration</Text>
+            <Text>{duration}</Text>
+            <Text c={'#7b7c88'}>Premiere</Text>
+            <Text>{premiereDate}</Text>
+            <Text c={'#7b7c88'}>Budget</Text>
+            <Text>{budget}</Text>
+            <Text c={'#7b7c88'}>Gross worldwide</Text>
+            <Text>{revenue}</Text>
+            <Text c={'#7b7c88'}>Genres</Text>
+            <Text>{genres}</Text>
+          </div>
         </Stack>
         {minInfo && <div className="movie-info-favorite">{<Favorites info={minInfo} />}</div>}
       </Group>
+
       <Space h={20} />
-      <Flex className="movie-info-card" direction="column" w={800}>
+
+      <Flex className="movie-info-card" direction="column" w={'100%'} maw={800}>
         {trailer ? (
           <>
             <Title order={3}>Trailer</Title>
             <Space h={16} />
-            <iframe
-              className="movie-info-trailer"
-              width="500"
-              height="281"
-              src={trailer}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
+            <AspectRatio ratio={16 / 9} maw={500}>
+              <iframe
+                className="movie-info-trailer"
+                src={trailer}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+            </AspectRatio>
             <Divider my="md" color={'#d5d6dc'} />
           </>
         ) : null}
